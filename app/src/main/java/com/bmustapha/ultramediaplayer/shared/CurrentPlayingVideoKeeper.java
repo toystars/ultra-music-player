5d9bc117b7139b10789a90c519cd8b1fe3d9003e
Mustapha Babatunde <toystars2008@gmail.com>
2015-11-09 00:29:56 +0100

    Change package name for new app on playstore

--- app/src/main/java/com/bmustapha/ultramediaplayer/shared/CurrentPlayingVideoKeeper.java
+++ app/src/main/java/com/bmustapha/ultramediaplayer/shared/CurrentPlayingVideoKeeper.java
@@ -6,7 +6,7 @@ import com.bmustapha.ultramediaplayer.models.Video;
  * Created by toystars on 11/8/15.
  */
 public class CurrentPlayingVideoKeeper {
-    
+
     public static Video currentPlayingVideo;
 
     public static void setCurrentPlayingVideo(Video video) {
